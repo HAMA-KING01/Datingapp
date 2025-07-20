1fc881a6e41c731de5e2f0b0ad3dc3ce72b28bbd
Mohammed Hama Salih <100773716+HAMA-KING01@users.noreply.github.com>
2025-07-20 17:13:32 +0300

    20-07-2025

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32937045256ddbd1667a67def51e1734939ad0be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f76a0ad96606ab10b7ed3d9cfce5f0953215c91")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
