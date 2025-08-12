3f599b2ea6ef2a461065c0b5d83b1635a2a41c26
Mohammed Hama Salih <100773716+HAMA-KING01@users.noreply.github.com>
2025-08-12 16:19:47 +0300

    section 7 -72

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbf17a5aaf44290040abbef93fba4b3473e39d51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd9d692c2a1c01f091d1036f0d141f379e70b66a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
